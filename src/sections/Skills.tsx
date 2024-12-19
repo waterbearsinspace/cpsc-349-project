61466e8143f67114dc89eb44ee0e3c2356be10b0
nicolasviste <nicolasviste@gmail.com>
2024-12-19 14:12:59 -0800

    Create Skills section layout

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,8 +1,45 @@
 function Skills() {
 
   return (
-    <section className="section">
-      <p>Skills Section.</p>
+    <section className="section" id="skills">
+      <div className="split">
+
+        <div>
+          <h2>Technical</h2>
+
+          <div className="split">
+            <div>
+              <div>Skill</div>
+              <div>Skill</div>
+              <div>Skill</div>
+            </div>
+            <div>
+              <div>Skill</div>
+              <div>Skill</div>
+              <div>Skill</div>
+            </div>
+          </div>
+
+          <h2>Non-Technical</h2>
+
+          <div className="split">
+            <div>
+              <div>Skill</div>
+              <div>Skill</div>
+              <div>Skill</div>
+            </div>
+            <div>
+              <div>Skill</div>
+              <div>Skill</div>
+              <div>Skill</div>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h1>Skills</h1>
+        </div>
+      </div>
     </section>
   )
 }
