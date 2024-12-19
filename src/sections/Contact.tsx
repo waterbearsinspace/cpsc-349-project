7087b528a3d9f6441b978d1469d57f179a734044
nicolasviste <nicolasviste@gmail.com>
2024-12-19 14:28:26 -0800

    Create Contact section layout

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -2,7 +2,18 @@ function Contact() {
 
   return (
     <section className="section contact">
-      <p>Contact Section.</p>
+      <h1>Contact</h1>
+      <form action="">
+        <label htmlFor="email">
+          <p>Email</p>
+        </label>
+        <input id="email" type="text" />
+
+        <label htmlFor="message">
+          <p>Message</p>
+        </label>
+        <textarea id="message" rows={25} cols={50}/>
+      </form>
     </section>
   )
 }
