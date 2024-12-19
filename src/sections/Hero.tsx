2dc30191f4dc95f67a43707d660dc3024260f531
nicolasviste <nicolasviste@gmail.com>
2024-12-19 13:57:19 -0800

    Create About section layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,14 +3,16 @@ import Avatar from "../assets/avatar.png"
 function Hero() {
 
   return (
-    <section className="section">
+    <section className="section" id="hero">
       <div className="split">
         <div className="split-lg">
           <div className="text">
             <h1>Hello. My name is Nick Viste.</h1>
+
             <p>I am a student at CSUF. I am currently finishing my degree in computer science and working to improve both my front end and back end skills. I know enough to know that I know very little and because of this, I am very excited about how much there is to learn and explore.</p>
           </div>
         </div>
+
         <div className="split-sm">
           <img src={Avatar} height="200 rem"></img>
         </div>
